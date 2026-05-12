c37909fd3c83742249318fe5d464604957dfd0f7
leeyujin0527 <sksdldbwls0527@gmail.com>
2026-05-12 09:35:04 +0900

    fix error

--- src/app/(root)/seat/page.tsx
+++ src/app/(root)/seat/page.tsx
@@ -2,40 +2,49 @@
 import SeatForm from "@/src/feature/seat/ui/seatForm";
 import { sessionStart } from "@/src/feature/seat/api/session-start";
 import { useSearchParams } from "next/navigation";
-import { useState } from "react";
+import { useState, Suspense } from "react";
 import { useRouter } from "next/navigation";
 import { useFlightStore } from "@/src/feature/flight/model/flightStore";
 import { getSession } from "@/src/feature/flight/api/session-get";
 import { startFocusSound } from "@/src/libs/flightSound";
 
-const seatPage = () => {
+const SeatContent = () => {
   const searchParams = useSearchParams();
   const destination = searchParams.get("destination");
   const origin = "Seoul";
   const [seat, setSeat] = useState<string>("");
   const flightName = searchParams.get("flightName");
+  const router = useRouter();
+
   if (!destination || !flightName) {
     return null;
   }
-  const router = useRouter()
+
   const handleStart = async () => {
     const res = await sessionStart({ flightName, origin, destination, seat });
     const session = await getSession(res.sessionId);
     useFlightStore.getState().setSession(session);
     startFocusSound();
-    router.push(`/flight?sessionId=${res.sessionId}`)
-    console.log(`Session started with flightName: ${flightName}, origin: ${origin}, destination: ${destination}, seat: ${seat}`);
-    console.log(res)
+    router.push(`/flight?sessionId=${res.sessionId}`);
   };
+
   return (
     <div>
       <SeatForm
         onStart={handleStart}
-        onSeatSelect ={setSeat}
+        onSeatSelect={setSeat}
         flightName={flightName}
       />
     </div>
   );
 };
 
-export default seatPage;
+const SeatPage = () => {
+  return (
+    <Suspense fallback={null}>
+      <SeatContent />
+    </Suspense>
+  );
+};
+
+export default SeatPage;
